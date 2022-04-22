e8734b3d62111403419e3d70779a7129b6403d41
KBS0093 <rhqjatjr0093@gmail.com>
2022-04-22 15:21:10 +0900

    Update main.kt

--- android/src/main/kotlin/com/yoonjaepark/flutter_naver_login/FlutterNaverLoginPlugin.kt
+++ android/src/main/kotlin/com/yoonjaepark/flutter_naver_login/FlutterNaverLoginPlugin.kt
@@ -190,18 +190,7 @@ class FlutterNaverLoginPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
         onFailure(errorCode, message)
       }
     }
-    
-    try {
-      OAUTH_CLIENT_ID = bundle?.getString("com.naver.sdk.clientId").toString();
-      OAUTH_CLIENT_SECRET = bundle?.getString("com.naver.sdk.clientSecret").toString();
-      OAUTH_CLIENT_NAME = bundle?.getString("com.naver.sdk.clientName").toString();
-      NaverIdLoginSDK.initialize(mContext!!, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME);
-      NaverIdLoginSDK.authenticate(this.getActivity()!!, mOAuthLoginHandler);
-    } catch (e: Exception) {
-      println(e)
-      onFailure(777, e)
-    }    
-    
+    NaverIdLoginSDK.authenticate(this.getActivity()!!, mOAuthLoginHandler);
   }
 
   fun logout(result: Result) {
